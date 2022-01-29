62168f1dd879181901f5574919a6ed6156a5afcf
Olsen <chrisolsen1993@gmail.com>
2022-01-29 08:07:41 -0500

    working on the click event

---
+++ src/components/utils/clickbars.js
@@ -0,0 +1,16 @@
+// $(".menu-btn").click(function(){
+//     $(".nav-link").toggleClass("active");
+//     $(".menu-btn i").toggleClass("active");
+// })
+// $(".a-nav").click(function(){
+//     $(".nav-link").toggleClass("active");
+//     $(".menu-btn i").toggleClass("active");
+// })
+
+export default function clickbar(){
+const menu = document.querySelector(".menu-btn");
+const aNav = document.querySelector(".a-nav");
+const navLink = document.querySelector(".nav-link");
+const menuBtn = document.querySelector(".menu-btn i")
+
+}
